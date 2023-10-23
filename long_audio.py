40ddf85d517e5301c614ad3faac621ed61e30c47
Leo Ngari <ngarimwenda@gmail.com>
2023-10-23 18:40:21 +0300

    added threading

--- long_audio.py
+++ long_audio.py
@@ -1,24 +1,37 @@
 import speech_recognition as sr
-
 from utils import AudioSplitter
 
 
+class Transcriber:
+    def __init__(self):
+        self.r = sr.Recognizer()
+
+    def transcribe(self, audio_file_loc: str):
+        splitter = AudioSplitter(audio_file_loc)
+        chunks = splitter.load_chunks()
+        res = ""
+        e = ""
+
+        for i, audio_chunk in enumerate(chunks):
+            audio_chunk.export("temp", format="wav")
+            with sr.AudioFile("temp") as source:
+                audio = self.r.listen(source)
+                try:
+                    text = self.r.recognize_google(audio)
+                    res += f" [{i}]{text} "
+
+                except Exception as ex:
+                    res += f"<<[e{i}>>"
+                    e += f"{ex.with_traceback}\n"
+
+        if res != "":
+            print(f"{res}")
+
+        if e != "":
+            print(f"[Errors] {e}")
+
+
 if __name__ == "__main__":
-    recognizer = sr.Recognizer()
     fn = "./samples/mp3/sample01.mp3"
-
-    splitter = AudioSplitter(fn)
-    chunks = splitter.load_chunks()
-
-    for i, audio_chunk in enumerate(chunks):
-        print(f"Chunk {i}: {audio_chunk}")
-        audio_chunk.export("temp", format="wav")
-        with sr.AudioFile("temp") as source:
-            audio = recognizer.listen(source)
-            try:
-                text = recognizer.recognize_google(audio)
-                print("Chunk : {}".format(text))
-
-            except Exception as ex:
-                print("Error occured")
-                print(ex)
+    t = Transcriber()
+    t.transcribe(fn)
--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
+from sys import getsizeof
+from re import T
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
+import concurrent.futures
+
+
+MAX_WORKERS = 5
 
 
 class AudioSplitter:
     DEFAULT_OFFSET = 10
-    DEFAULT_MIN_SILENCE_LEN = 1000
+    DEFAULT_MIN_SILENCE_LEN = 2000
+    DEFAULT_MAX_N_OF_CHUNKS = 10
 
     def __init__(self, file: str) -> None:
         self.file = file
@@ -15,22 +22,21 @@ class AudioSplitter:
         min_silence_len: int,
         silence_thresh: int,
         keep_silence=200,
-        verbose=False,
     ) -> list:
-        audio_chunks = split_on_silence(
+        return split_on_silence(
             audio_segment=self.audio,
             min_silence_len=min_silence_len,
             silence_thresh=silence_thresh,
             keep_silence=keep_silence,
         )
-        if verbose:
-            print(
-                f"Audio splitting into chunks complete! Number of chunks: {len(audio_chunks)}"
-            )
-        return audio_chunks
+
+    def _get_average_chunk_size(self, chunks: list):
+        if len(chunks) == 0:
+            return None
+        return sum([getsizeof(obj) for obj in chunks]) / len(chunks)
 
     def load_chunks(
-        self, offset=DEFAULT_OFFSET, min_silence_len=DEFAULT_MIN_SILENCE_LEN
+        self, min_silence_len=DEFAULT_MIN_SILENCE_LEN, verbose=True
     ) -> list:
         """
         Splits mp3 files into chunks
@@ -43,16 +49,46 @@ class AudioSplitter:
         list[AudioSegment]
         """
 
-        # TODO: Spin up multiple threads since self.audio is immutable
+        if verbose:
+            print("[Info] Converting audio into chunks")
 
-        opt_silence_thresh = 0
+        opt_chunks = []
         highest_chunk_count = 0
 
-        for i in range(1, 100, offset):
-            silence_thresh = -(i + offset)
-            chunks = self._load_chunks(min_silence_len, (i + offset))
-            if len(chunks) > highest_chunk_count:
-                highest_chunk_count = len(chunks)
-                opt_silence_thresh = silence_thresh
+        with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+            future_to_offset = {
+                executor.submit(
+                    self._load_chunks,
+                    min_silence_len,
+                    -offset,
+                )
+                for offset in range(1, 100, self.DEFAULT_OFFSET)
+            }
+            for i, future in enumerate(
+                concurrent.futures.as_completed(future_to_offset)
+            ):
+                chunks = future.result()
+                try:
+                    size = len(chunks)
+                    if verbose:
+                        print(
+                            f"[Info] Phase_{i+1}\nNumber of chunks: {size}.\nAverage chunk size: {self._get_average_chunk_size(chunks)}bytes.\n\n"
+                        )
 
-        return self._load_chunks(min_silence_len, opt_silence_thresh, verbose=True)
+                    if (
+                        size > highest_chunk_count
+                        and size <= self.DEFAULT_MAX_N_OF_CHUNKS
+                    ):
+                        highest_chunk_count = size
+                        opt_chunks = chunks
+
+                except Exception as exc:
+                    print(
+                        f"[Error] Something went wrong while splitting chunks. See error msg: {exc}"
+                    )
+
+        if verbose:
+            print(
+                f"[Info] Audio splitting into chunks complete! Number of chunks: {len(opt_chunks)}"
+            )
+        return opt_chunks
