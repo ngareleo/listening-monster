92aa7585756b87e79de8f13ceef8ca6d5fb80df6
Leo Ngari <ngarimwenda@gmail.com>
2023-10-30 20:32:40 +0300

    add file upload

--- source/server/routes/upload.py
+++ source/server/routes/upload.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from fileinput import filename
+from flask import Blueprint, current_app, flash, render_template, request, url_for
 from ..utils import login_required
+import os
 
 
 bp = Blueprint("upload", __name__, url_prefix="/upload")
@@ -7,5 +9,17 @@ bp = Blueprint("upload", __name__, url_prefix="/upload")
 
 @login_required
 @bp.route("/", methods=("GET", "POST"))
-def upload():
+def index():
+    if request.method == "POST":
+        f = request.files["audio"]
+        if not current_app.static_folder:
+            return "<p>Something went wrong</p>"
+
+        location = f"/audio/{f.filename}"
+        f.save(f"{current_app.static_folder}/{location}")
+        flash("Image uploaded successfully 💾")
+        return render_template(
+            "sections/audio.html",
+            location=url_for("static", filename=location),
+        )
     return render_template("upload.html")
