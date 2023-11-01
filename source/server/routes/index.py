8026ce5b99ed6f315da0f475e641475ca17fbe25
Leo Ngari <ngarimwenda@gmail.com>
2023-11-01 21:42:53 +0300

    add page layout

--- source/server/routes/index.py
+++ source/server/routes/index.py
@@ -7,5 +7,5 @@ bp = Blueprint("index", __name__, url_prefix="/")
 @bp.route("/", methods=("GET", "POST"))
 def hello_traveller():
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("pages/index.html")
     return "sent"
--- source/server/routes/upload.py
+++ source/server/routes/upload.py
@@ -1,7 +1,5 @@
-from fileinput import filename
 from flask import Blueprint, current_app, flash, render_template, request, url_for
 from ..utils import login_required
-import os
 
 
 bp = Blueprint("upload", __name__, url_prefix="/upload")
@@ -11,15 +9,17 @@ bp = Blueprint("upload", __name__, url_prefix="/upload")
 @bp.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        f = request.files["audio"]
-        if not current_app.static_folder:
+        f = request.files.get("audio")
+        if not f or not current_app.static_folder:
             return "<p>Something went wrong</p>"
 
         location = f"/audio/{f.filename}"
         f.save(f"{current_app.static_folder}/{location}")
         flash("Image uploaded successfully 💾")
+
         return render_template(
             "sections/audio.html",
             location=url_for("static", filename=location),
         )
+    
     return render_template("upload.html")
